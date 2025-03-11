66ba5d365b3a7947d0a696cdead60a05b3c26fd4
haritsrhn <haritsraihan75@gmail.com>
2025-03-11 20:04:55 +0700

    implement react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import AppTheme from "./shared-theme/AppTheme";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-// import Latest from './components/Latest';
+// import Latest from "./components/Latest";
 import Footer from "./components/Footer";
 
 export default function App(props) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
-      <Header />
-      <Container
-        maxWidth="lg"
-        component="main"
-        sx={{ display: "flex", flexDirection: "column", my: 16, gap: 4 }}
-      >
-        <MainContent />
-        {/* <Latest /> */}
-      </Container>
-      <Footer />
+      <Router>
+        <Header />
+        <Container
+          maxWidth="lg"
+          component="main"
+          sx={{ display: "flex", flexDirection: "column", my: 16, gap: 4 }}
+        >
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            {/* <Route path="/latest" element={<Latest />} /> */}
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
     </AppTheme>
   );
 }
